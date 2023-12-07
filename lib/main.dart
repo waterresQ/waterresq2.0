e6d49fad3a1937233d23754989247baca9e376c5
Vasithra <vasithra11@gmail.com>
2023-12-07 22:59:00 +0530

    Vasithra

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sihwaterresq/Screens/home.dart';
 import 'package:sihwaterresq/Screens/loading.dart';
 import 'package:sihwaterresq/Screens/login.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
